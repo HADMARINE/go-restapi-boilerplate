54eeeda9c5a00d654d2fded2653e587bc3751d04
HADMARINE <contact@hadmarine.com>
2020-05-03 20:05:28 +0900

    create ReadJSON

---
+++ src/util/ReadJSON.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// ReadJSON reads JSON file and returns the value
+func ReadJSON(filename string) map[string]interface{} {
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+
+	return result
+}
