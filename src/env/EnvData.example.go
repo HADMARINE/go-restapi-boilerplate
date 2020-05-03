889ce9faef8589074d6043b5924dfd5c725961e5
HADMARINE <contact@hadmarine.com>
2020-05-03 20:05:22 +0900

    refactor env settings

--- src/env/EnvData.example.go
+++
@@ -1,6 +0,0 @@
-package env
-
-// Data returns environment variables
-func Data() map[string]string {
-	return map[string]string{"PORT": "60020", "GIN_MODE": "debug"}
-}
--- src/env/index.go
+++ src/env/index.go
@@ -1,12 +1,13 @@
 package env
 
 import (
+	"go-restapi-boilerplate/src/util"
 	"os"
 )
 
 // Init function sets env vars.
 func Init() {
-	for key, value := range Data() {
-		os.Setenv(key, value)
+	for key, value := range util.ReadJSON(".env.json") {
+		os.Setenv(key, value.(string))
 	}
 }
