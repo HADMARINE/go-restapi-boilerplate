7eeec212bb572b719100496d4a0bb1cae934dd02
HADMARINE <contact@hadmarine.com>
2020-05-05 22:58:24 +0900

    Create User model

---
+++ models/User.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"github.com/zebresel-com/mongodm"
+)
+
+// User is model of User
+type User struct {
+	mongodm.DocumentBase `json:",inline" bson:",inline"`
+
+	Userid    string `json:"userid" bson:"userid"`
+	Password  string `json:"password" bson:"password"`
+	Enckey    string `json:"enckey" bson:"enckey"`
+	Authority string `json:"authority" bson:"authority"`
+}
